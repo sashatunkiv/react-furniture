b5418603e58c9216d1c4651b21cf6b3dd20dd79f
Oleksandr <113296379+sashatunkiv@users.noreply.github.com>
2024-02-13 19:44:00 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, HashRouter } from 'react-router-dom';
 import './index.scss';
 import 'macro-css';
 import App from './App';
@@ -8,8 +8,8 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
+    <HashRouter>
       <App />
-    </Router>
+    </HashRouter>
   </React.StrictMode>,
 );
